4caf892d8c6ad6516f5dbb4b1bc5e0d364672e7e
Oliver Gruenzel <oliver@gruenzel.de>
2024-03-12 22:07:04 +0100

    german keymap, rev 5

--- config/key_de.h
+++ config/key_de.h
@@ -1,14 +1,2 @@
-/*
- * Copyright (c) 2020 The ZMK Contributors
- *
- * SPDX-License-Identifier: MIT
- */
-#pragma once
-
-#include <dt-bindings/zmk/hid_usage.h>
-#include <dt-bindings/zmk/hid_usage_pages.h>
-#include <dt-bindings/zmk/modifiers.h>
-#include <dt-bindings/zmk/keys.h>
-
 // Aliases
 #define DE_CIRC GRAVE  // ^ (dead)

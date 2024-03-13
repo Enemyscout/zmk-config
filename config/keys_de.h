6bbff6157a7113e9bb1f8a9aa424cb13372d895f
Enemyscout <122307906+Enemyscout@users.noreply.github.com>
2024-03-13 11:31:49 +0100

    Update keys_de.h

--- config/keys_de.h
+++ config/keys_de.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <behaviors.dtsi>
-#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
 #include <dt-bindings/zmk/keys.h>
-
 
 // Aliases
 #define DE_CIRC GRAVE  // ^ (dead)
